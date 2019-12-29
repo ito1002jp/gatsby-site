16b49dec3e83c3ce1b48d929421dbbb6e6574f5d
Ohkura Tessyuu <ito1002jp@gmail.com>
2019-12-29 14:52:32 +0900

    tutorial-part-four

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,13 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
+  siteMetadata: {
+    title: `Pamdas Eating Lots`
+  },
   plugins: [
+    `gatsby-plugin-emotion`,
     {
-      resolve : 'gatsby-plugin-typography',
-      options : {
-        pathToConfigModule : `src/utils/typography`,
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
   ],
