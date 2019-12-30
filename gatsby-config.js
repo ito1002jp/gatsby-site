5b90b8bf478d4d7dd4b8e25899d21cb8f4bf021c
Ohkura Tessyuu <ito1002jp@gmail.com>
2019-12-30 11:47:55 +0900

    tutorial-part-five

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,13 @@ module.exports = {
     title: `Pamdas Eating Lots`
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
